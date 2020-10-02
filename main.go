c19dfd931bb83fa31a288123ca1248f61e8ce0d5
Ryu <Rewapk@gmail.com>
2020-10-02 16:03:13 +0700

    Add Css

--- main.go
+++ main.go
@@ -13,9 +13,11 @@ type Page struct {
 }
 
 func main() {
-	welcome := Page{"User 1", time.Now().Format(time.Stamp)}
+	welcome := Page{"Annonymus", time.Now().Format(time.Stamp)}
 
 	templates := template.Must(template.ParseFiles("Template/template.html"))
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		if name := r.FormValue("name"); name != "" {
