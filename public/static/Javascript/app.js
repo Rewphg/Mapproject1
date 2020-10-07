1a800cc136b174dbc896fd48eb6fa429a3408c0e
Ryu <Rewapk@gmail.com>
2020-10-07 10:57:40 +0700

    Add database Setup

--- public/static/Javascript/app.js
+++ public/static/Javascript/app.js
@@ -1,18 +1,25 @@
-(function () {
-    var config = {
-        apiKey: "AIzaSyB-OBk-Q2Ik7jqRp9XlTCLIL8FPbUhozUA",
-        authDomain: "mapproject1-76680.firebaseio.com",
-        databaseURL: "https://mapproject1-76680.firebaseio.com/",
-        storageBucket: "mapproject1-76680.appspot.com"
-      };
-    firebase.initializeApp(config);
-    
-    function writeFirebaseData(Data, Index) {
-        firebase.database().ref().child('Data'){
-            type: Data.type,
-            x: Data.x,
-            y: Data.y,
-        });
-        console.log("writeData")
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+    apiKey: "AIzaSyB-OBk-Q2Ik7jqRp9XlTCLIL8FPbUhozUA",
+    authDomain: "mapproject1-76680.firebaseapp.com",
+    databaseURL: "https://mapproject1-76680.firebaseio.com",
+    projectId: "mapproject1-76680",
+    storageBucket: "mapproject1-76680.appspot.com",
+    messagingSenderId: "845059379868",
+    appId: "1:845059379868:web:b027db02ef397c849f5359",
+    measurementId: "G-WB26Y8VMXC"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+function SaveData() {
+    for (i = 0; i < Marker.length; i++) {
+        var D = Marker[i]
+        Mname = "Marker" + D.Number
+        writeFirebaseData(D, Mname)
+        console.log("save")
     }
-  })
+}
--- public/static/Javascript/main.js
+++ public/static/Javascript/main.js
@@ -106,31 +106,3 @@ window.addEventListener("resize" , function(){
     canvas.fillStyle = '#00FFF'
     canvas.fillRect(0,0)
 }, false)
-
-var config = {
-    apiKey: "AIzaSyB-OBk-Q2Ik7jqRp9XlTCLIL8FPbUhozUA",
-    authDomain: "mapproject1-76680.firebaseio.com",
-    databaseURL: "https://mapproject1-76680.firebaseio.com/",
-    storageBucket: "mapproject1-76680.appspot.com"
-  };
-firebase.initializeApp(config);
-
-var database = firebase.database();
-
-function writeFirebaseData(Data, Index) {
-    firebase.database().ref('Marker/' + Index).set({
-        type: Data.type,
-        x: Data.x,
-        y: Data.y,
-    });
-    console.log("writeData")
-}
-
-function SaveData() {
-    for (i = 0; i< Marker.length; i++){
-        var D = Marker[i]
-        Mname = "Marker" + D.Number
-        writeFirebaseData(D, Mname)
-    }
-  }
-document.getElementById("SaveBut").addEventListener('click', SaveData(Marker))
