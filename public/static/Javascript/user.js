6c798a382749ba40a36297457eae7a469d1ed817
Thanapat Eurboonyanun <champ_tnt@hotmail.com>
2020-11-13 00:19:18 +0700

    Finish dynamic programming for rendering page but UI not finish

---
+++ public/static/Javascript/user.js
@@ -0,0 +1,236 @@
+//Reference: Stackoverflow, TowardDataScience, Reddit Forum
+
+const MPos = [{
+    x: 0,
+    y: 0,
+}]
+
+// function setupCanvas(canvas) {
+//     var dpr = window.devicePixelRatio || 1;
+//     var rect = canvas.getBoundingClientRect();
+//     canvas.width = rect.width * dpr;
+//     canvas.height = rect.height * dpr;
+//     var ctx = canvas.getContext('2d');
+//     ctx.scale(dpr, dpr);
+//     return ctx;
+//   }
+// var ctx = setupCanvas(document.querySelector('.my-canvas'));
+
+const canvas = document.querySelector("#canvas")
+const ctx = canvas.getContext('2d')
+
+var mode = 0
+
+let route = false;
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const Background = []
+const BoothIcons = []
+const ToiletIcons = []
+const Infos = []
+const lines = []
+
+// var object = {
+//     "toilet": [],
+//     "booth": [],
+//     "info": [],
+//     "route": [],
+//     "map": [],
+// }
+
+var arr_object = []
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    if (Background.length > 0) {
+        Background[Background.length - 1].Draw()
+    }
+    requestAnimationFrame(animate)
+    var OldX;
+    var OldY;
+    arr_object.forEach((BoothIcon, index) => {
+        if (BoothIcon.hasOwnProperty("type")) {
+            ctx.beginPath();
+            ctx.lineWidth = 5;
+            ctx.lineCap = "round";
+            ctx.moveTo(OldX, OldY);
+            ctx.lineTo(BoothIcon.x, BoothIcon.y);
+            ctx.strokeStyle = '#FF0000'
+            ctx.stroke();
+            OldX = BoothIcon.x;
+            OldY = BoothIcon.y;
+        } else {
+            //console.log(arr_object);
+            BoothIcon.Update()
+        }
+        /*if (CheckCollitionImg(BoothIcon, Eraser) == true) {
+            BoothIcons.splice(BoothIcon, 1)
+        }*/
+    });
+      
+} // .End animate
+
+function startRoute(e) {
+    route = true;
+    draw(e);
+}
+
+function endRoute() {
+    route = false;
+    ctx.beginPath();
+}
+
+function draw(e) {
+    if (!route) return;
+    ctx.lineWidth = 5;
+    ctx.lineCap = "round";
+
+    ctx.lineTo(e.clientX, e.clientY);
+    ctx.strokeStyle = "#FF0000";
+    ctx.stroke();
+}
+
+canvas.addEventListener("mousedown", startRoute);
+canvas.addEventListener("mouseup", endRoute);
+canvas.addEventListener("mousemove", draw);
+
+var ConX = 0
+var ConY = 0
+var On = 0
+var EditIndex = 0
+
+//mode 0 = none ,mode 1 = Booth ,mode 2 = Toilet ,mode 3 = info, mode 4 = eraser, mode 5 = route
+
+document.getElementById("canvas").addEventListener("click", (event) => {
+    event.preventDefault()
+    var border = document.getElementById("canvas").getBoundingClientRect();
+    MPos.x = event.clientX - border.left - 25
+    MPos.y = event.clientY - border.top - 25
+    mode = 7
+    console.log(MPos.x, MPos.y)
+    if (mode == 7) {
+
+        arr_object.forEach((A, index) => {
+            if (CheckCollition(MPos.x, MPos.y, A) == true) {
+                //BoothIcons.splice(index, 1)
+                //OpenEdit(arr_object[index])
+                document.getElementById("info").style = "display: block";
+                EditIndex = index
+
+                qr.set({
+                    foreground: 'black', //  setup background color of qr code.
+                    size: 100, // size image qr code
+                    value: arr_object[index].title + "," + arr_object[index].dis + "," + arr_object[index].x + "," + arr_object[index].y // set text for qr
+                });
+
+            }
+        });
+    }
+
+    if (mode == 4) {
+        arr_object.forEach((A, index) => {
+            if (CheckCollition(MPos.x, MPos.y, A) == true) {
+                arr_object.splice(index, 1)
+                    /*OpenEdit(BoothIcons[index])
+                    //EditIndex = index
+
+                    qr2.set({
+                        foreground: 'black', //  setup background color of qr code.
+                        size: 100, // size image qr code
+                        value: BoothIcons[index].title + "," + BoothIcons[index].dis + "," + BoothIcons[index].x + "," + BoothIcons[index].y   // set text for qr
+                    });
+                    */
+            }
+        });
+
+        arr_object.forEach((A, index) => {
+            if (CheckCollition(MPos.x, MPos.y, A) == true) {
+                OpenEdit(arr_object[index])
+                EditIndex = index
+
+                qr2.set({
+                    foreground: 'black', //  setup background color of qr code.
+                    size: 100, // size image qr code
+                    value: arr_object[index].title // set text for qr
+                });
+            }
+        });
+        arr_object.forEach((B, index) => {
+            if (CheckCollition(MPos.x, MPos.y, B) == true) {
+                arr_object.splice(index, 1)
+            }
+        });
+        arr_object.forEach((B, index) => {
+            if (CheckCollition(MPos.x, MPos.y, B) == true) {
+                arr_object.splice(index, 1)
+            }
+        });
+    }
+
+    if (MPos.y < canvas.height && MPos.x < canvas.width) {
+        On = 1
+        if (mode == 1) {
+            ConX = event.clientX - border.left - 25
+            ConY = event.clientY - border.top - 25
+            var index = 1
+            ShowMyForm(arr_object, index)
+        }
+
+        if (mode == 2) {
+            // object.toilet.push(new BoothIcon(MPos.x, MPos.y, 50, 50, "/static/Icons/toilet.png", "Toilet"))
+            arr_object.push(new BoothIcon(MPos.x, MPos.y, 50, 50, "/static/Icons/toilet.png", "Toilet"))
+            //console.log(arr_object);
+        }
+
+        if (mode == 3) {
+            arr_object.push(new BoothIcon(MPos.x, MPos.y, 50, 50, "/static/Icons/info.png", "Info"))
+            //console.log(arr_object);
+        }
+
+        if (mode == 5) {
+            arr_object.push({ x: MPos.x, y: MPos.y, "type": 'line' });
+            //console.log(arr_object);
+            //console.log(arr_object[0].type)
+        }
+
+    }
+})
+
+function initObject(arr) {
+    new_arr = []
+    arr.forEach(elem => {
+        if (elem['type'] != 'line') {
+            new_arr.push(new BoothIcon(elem.x, elem.y, elem.width, elem.height, elem.src, elem.title))
+        } else {
+            new_arr.push(elem)
+        }
+
+    })
+    return new_arr
+
+}
+
+document.getElementById("sent").addEventListener("click", function(event) {
+    //var user = '{{username}}'
+    //var pid = '{{pid}}'
+    xmlObj = new XMLHttpRequest();
+    xmlObj.open("POST", "http://localhost:5000/org/" + user + "/project/" + pid + "/save", true);
+    xmlObj.setRequestHeader("Content-Type", "application/json");
+    var data = JSON.stringify({ "object": arr_object });
+    xmlObj.send(data);
+    xmlObj.onreadystatechange = handleRequest();
+
+    function handleRequest() {
+        if (xmlObj.readyState == 4 && xmlObj.status == 200) {
+            var myJSON = JSON.parse(xmlObj.responseText);
+            document.getElementById("response").innerHTML = myJSON.prediction;
+            alert("loaded");
+        } else {
+            alert(xmlObj.status);
+        }
+    }
+})
+
+animate()
---
+++ public/templates/toHTML.js
@@ -0,0 +1,186 @@
+// //Test zone
+// var test = [
+//     {
+//         "toilet":[
+//             {
+//                 "x":"55.0",
+//                 "y":"78.0"
+//             }
+//         ],
+//         "booth":[
+//             {
+//                 "x":"79.0",
+//                 "y":"155.0",
+//                 "boothname":"Hello, World",
+//                 "boothdescription":"Tummai"
+//             }
+//         ],
+//         "info":[
+//             {
+//                 "x":"102.0",
+//                 "y":"222.0"
+//             }
+//         ],
+//         "route":[
+//             {
+//                 "x":"100.0",
+//                 "y":"325.0"
+//             },
+//             {
+//                 "x":"12.0",
+//                 "y":"212.0"
+//             }
+//         ],
+//         maptemplate:"https://yt3.ggpht.com/a/AATXAJywokR9YldORmSqcR0rgcqeH94IYEM9fNP2-SmHpA=s900-c-k-c0x00ffffff-no-rj"
+//     }
+// ]
+
+// const { pid } = require("process");
+
+// const { pid } = require("process");
+
+// //Real zone
+// document.getElementById("app").innerHTML = `
+// <h1 class="project-title">(${test.length})</h1>
+// ${test.map(function(template) {
+//     return `
+//     <div class="map">
+//         <img class="map-size" src="${test[0].maptemplate}">
+//     </div>
+//     `
+// }).join("")}
+// `
+
+// var user = document.getElementById("user")
+// var ctxuser = user.getContext("2d");
+
+var pid = `{{pid}}`
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.open("GET", "http://localhost:5000/user/" + pid + "/json", true);
+xmlhttp.setRequestHeader("Content-Type", "application/json");
+xmlhttp.send();
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var data = JSON.parse(this.responseText);
+        renderData(data.object);
+        console.log(data)
+    }
+}
+
+            var arr_object = [];
+            var current_booth;
+            // var user = '{{username}}'
+            
+            var qr;
+
+            var savedContent = new Object();
+
+            (function() {
+                qr = new QRious({
+                    element: document.getElementById('qr-code'), // canvas object
+                    size: 100, // size image qr code
+                    value: 'Event Me!!!' // value default text
+                });
+
+                doRenderTemp();
+                
+            })();
+
+            function generateQRCode() {
+                var qrtext = document.getElementById("qr-text").value; // value  text from text box
+                //document.getElementById("qr-result").innerHTML = "QR code for " + qrtext +":"; // get element p object to assign text
+                //alert(qrtext); 
+                qr.set({
+                    foreground: 'black', //  setup background color of qr code.
+                    size: 100, // size image qr code
+                    value: qrtext // set text for qr
+                });
+            }
+
+            // function refreshServer() {
+            //     $.getJSON("/user/{{pid}}/json", function(d) {
+            //         //console.log(d.object);
+            //         // Put the object into storage
+            //         if (localStorage.getItem('mapObject') != null) {
+            //             localStorage.setItem('mapsObject', JSON.stringify(d.object));
+            //         }
+            //         renderData(d.object);
+                    
+            //     }).fail(function() {
+            //         console.log("An error has occurred.");
+            //     });
+            // }
+
+            function clearMap() {
+                arr_object = [];
+                saveTemp();
+                doRenderTemp()
+            }
+
+            function saveTemp() {
+                // Put the object into storage
+                var tempData = arr_object
+
+                localStorage.setItem('mapsObject', JSON.stringify(tempData));
+            }
+
+            function renderData(data) {
+                    var OldX;
+                    var OldY;
+                    data.forEach((el, index) => {
+                        
+                        if (el.hasOwnProperty("type")){
+                            console.log(el.type)
+                            arr_object.push({x:el.x,y:el.y, "type": "line"});
+                            console.log(el.x, el.y)
+                        }
+                    })
+                
+
+                    data.forEach(function(el,i) {
+                        if (el.hasOwnProperty("src") && (el.src == "/static/Icons/pin.png" || el.src == "../../../static/Icons/pin.png")){
+                            arr_object.push(new BoothIcon(el.x, el.y, 50, 50, "../../../static/Icons/pin.png", el.title))
+                        }
+                    });
+
+                    data.forEach(function(el,i) {
+                        if (el.hasOwnProperty("src") && (el.src == "/static/Icons/info.png" || el.src == "../../../static/Icons/info.png")){
+                            arr_object.push(new BoothIcon(el.x, el.y, 50, 50, "../../../static/Icons/info.png", el.title))
+                        }
+                    });
+
+                    data.forEach(function(el,i) {
+                        if (el.hasOwnProperty("src") && (el.src == "/static/Icons/toilet.png" || el.src == "../../../static/Icons/toilet.png" )){
+                            arr_object.push(new BoothIcon(el.x, el.y, 50, 50, "../../../static/Icons/toilet.png", el.title))
+                        }
+                    });
+
+                    
+            } // load data
+
+            function readTemp() {
+                // Retrieve the object from storage
+
+                var retrievedObject = JSON.parse(localStorage.getItem('mapsObject')); //JSON.stringify());
+                if (retrievedObject != null && retrievedObject.length > 0) {
+                    return retrievedObject;
+                }
+                return [];
+
+            }
+
+            function doRenderTemp() {
+                renderData(readTemp());
+            }
+
+            function updateQrCode(){
+                var title = $("#DisName").val();
+                var desc = $("#Description").val();
+                var new_x = MPos.x;
+                var new_y = MPos.y;
+                qr.set({
+                    foreground: 'black', //  setup background color of qr code.
+                    size: 100, // size image qr code
+                    value: title + "," + desc + "," + new_x + "," + new_y // set text for qr
+                });
+            }
